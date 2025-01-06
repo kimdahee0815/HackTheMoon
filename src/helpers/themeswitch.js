219797242b64340da5604fc010a9b6e82e8f7e03
kimdahee0815 <billionairedahee@gmail.com>
2025-01-06 22:51:43 +0900

    feat: light mode and dark mode both created

--- src/helpers/themeswitch.js
+++ src/helpers/themeswitch.js
@@ -1,9 +1,9 @@
 document.getElementById('theme-switch').addEventListener('click', function() {
   if(document.body.classList.contains('theme-dark')){
     console.log('theme-dark')
-    return "/src/site/_includes/layouts/lightthemeindex.njk";
+    
   }else if(document.body.classList.contains('theme-light')){
     console.log('theme-light')
-    return "/src/site/_includes/layouts/index.njk";
+   
   }
 })
