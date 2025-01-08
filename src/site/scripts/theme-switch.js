2400c2a41da48768bf1a0f775ecd0367237878d2
kimdahee0815 <billionairedahee@gmail.com>
2025-01-08 13:54:38 +0900

    fix: auto icon not changed for index, note

--- src/site/scripts/theme-switch.js
+++
@@ -1,9 +0,0 @@
-// document.getElementById('theme-switch').addEventListener('click', function() {
-//   if(window.__theme__ === 'dark' || !window.__theme__){
-//     return "layouts/index.njk";
-//   }else if(window.__theme__ === 'light'){
-//     return "layouts/lightthemeindex.njk";
-//   }else{
-//     return "layouts/index.njk";
-//   }
-// })
